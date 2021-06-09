850184d8a3b759b655f32395cf7c547614169f6c
Ricardo Schleder Tozetto <ricardo@optim.eng.br>
2021-06-09 11:14:54 -0300

    Commit Inicial

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+
+//app.ts
+import express from 'express'
+import bodyParser from 'body-parser'
+import indexRoutes from './routes/index'
+
+const app = express();
+app.use(bodyParser.json());
+app.use(indexRoutes);
+app.listen(3000);
